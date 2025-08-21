7417967d12a03538874fdadabf662b568a61fdeb
Nathan <an.email.com>
2025-08-21 01:43:03 +0100

    polish

--- src/union-registry-builder.ts
+++ src/union-registry-builder.ts
@@ -1,7 +1,7 @@
 import { AsUnionMetadata, UnionMetadata } from './utility-types';
 
 export type UnionRegistryBuilder<TUnionRegistry extends readonly UnionMetadata<any, any>[]> = {
-  register: <TBase extends Record<string, any>, TUnion extends TBase>
+  register: <TBase, TUnion extends TBase>
     () => AsUnionMetadata<TUnionRegistry, TBase> extends never
     ? UnionRegistryBuilder<[...TUnionRegistry, UnionMetadata<TBase, TUnion>]>
     : 'Base type is duplicated which would prevent it\'s sub types being available. Alter the base type structure to distinguish it.';
@@ -10,7 +10,7 @@ export type UnionRegistryBuilder<TUnionRegistry extends readonly UnionMetadata<a
 
 export const unionRegistryBuilderInternal = <TUnionRegistry extends readonly UnionMetadata<any, any>[]>(entries: TUnionRegistry): UnionRegistryBuilder<TUnionRegistry> => {
   return {
-    register: <TBase extends Record<string, any>, TUnion extends TBase>() => {
+    register: <TBase, TUnion extends TBase>() => {
       return unionRegistryBuilderInternal([...entries, null as any as UnionMetadata<TBase, TUnion>] as const) as any;
     },
     build: () => entries,
