cc8618d0dfe24eac7dc76844b7b8c49c9f38e5f9
Nathan <an.email.com>
2025-08-24 11:37:15 +0100

    polish error message

--- src/constants.ts
+++ src/constants.ts
@@ -1,7 +1,7 @@
 export const errorMessages = {
   notValid: 'Primitives, any, unknown, never, function, empty object/tuple types cannot be built. Please choose another type.',
   notBuildable: 'Internal Error: Type is valid but cannot currently be built. Please raise an issue at https://github.com/NathanJAdams/ts-fluent-builder/issues and include the type causing this error. Thank you.',
-  notConsistent: 'When using a union, types must all be valid and consistent ie. only arrays, only records or only objects.',
+  notConsistent: 'When building a top-level union, types must all be valid and consistent ie. only arrays, only records or only objects.',
 } as const;
 
 export const suffixes = {
